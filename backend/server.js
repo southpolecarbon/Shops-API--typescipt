69f8e511b0fa0da914032aac7bfda3d9c3977d5d
Farnaz Rashidi <farnazrashidi@SPC-C02K64AKQ6LR.local>
2024-09-04 12:50:01 +0200

    feat: set req headers in frontend

--- backend/server.js
+++ backend/server.js
@@ -14,23 +14,7 @@ app.post("/graphql", async (req, res) => {
   try {
     console.log("Received GraphQL request:", JSON.stringify(req.body, null, 2));
 
-    const isGetProductsQuery = req.body.operationName === "GetProducts";
-
-    // Extract the Authorization header from the incoming request
-    const authHeader = req.headers.authorization;
-
-    // NOTE: the required header's name is different for GetProductsQuery vs the rest of queries/mutations
-    const headers = {
-      "Content-Type": "application/json",
-      ...(isGetProductsQuery
-        ? { "Magento-Store-View-Code": process.env.STORE_VIEW_CODE }
-        : { Store: process.env.STORE_VIEW_CODE }),
-      ...(authHeader && { Authorization: authHeader }), // Include the Authorization header if it exists
-    };
-
-    const response = await axios.post(process.env.GQL_API_URL, req.body, {
-      headers,
-    });
+    const response = await axios.post(process.env.GQL_API_URL, req.body);
     console.log("GraphQL response:", JSON.stringify(response.data, null, 2));
     res.json(response.data);
   } catch (error) {
